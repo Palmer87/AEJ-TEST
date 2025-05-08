e657e732d007e4d523c581d3cf97e30f48264a61
djue <djuekouassicelestin@gmail.com>
2025-05-08 16:48:03 +0000

    feat:dasboard addmin et validation de projet

--- app/Models/Projet.php
+++ app/Models/Projet.php
@@ -4,6 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $promoteur_id
+ * @property string $titre
+ * @property string $type_projet
+ * @property string $forme_juridique
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $plan_affaires
+ * @property-read \App\Models\Promoteur $promoteur
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereFormeJuridique($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet wherePlanAffaires($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet wherePromoteurId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereTitre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereTypeProjet($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Projet whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Projet extends Model
 {
     protected $fillable = [
--- app/Models/Promoteur.php
+++ app/Models/Promoteur.php
@@ -4,6 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $utilisateur_id
+ * @property string $date_naissance
+ * @property string $lieu_naissance
+ * @property string $numero_cni
+ * @property string $cni_image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Projet> $projets
+ * @property-read int|null $projets_count
+ * @property-read Promoteur|null $promoteur
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereCniImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereDateNaissance($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereLieuNaissance($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereNumeroCni($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Promoteur whereUtilisateurId($value)
+ * @mixin \Eloquent
+ */
 class Promoteur extends Model
 {
     protected $fillable = ['utilisateur_id', 'date_naissance', 'lieu_naissance', 'numero_cni', 'cni_image'];
